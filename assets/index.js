6384fd61024a24fcc1f39f2b594772f1b575ec3c
aggabaorenz16 <43602006+aggabaorenz16@users.noreply.github.com>
2022-06-20 17:26:53 +0800

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,18 @@
+// const card1 = document.querySelector("#card1");
+// const card2 = document.querySelector("#card2");
+// const card3 = document.querySelector("#card3");
+// const nextpage = document.querySelector("#next");
+// const prevpage = document.querySelector("#prev");
+
+// const next = () => {
+//     if(nextpage.clicked = true) {
+//         card1.style.display = 'none'
+//         card2.style.display = 'block'
+//     }
+// }
+// const prev = () => {
+//     if(prevpage.clicked = true) {
+//         card1.style.display = 'block'
+//         card2.style.display = 'none'
+//     }
+// }
